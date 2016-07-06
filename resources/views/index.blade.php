0ab84c4475bf281e5dd4a97b9a3bc2f4a6191314
topin <topin@efexi.cl>
2016-07-06 00:17:13 -0400

    arreglos en index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,37 +5,22 @@
 {{-- <div class="right-content" style="margin-left: -250px;"> --}}
 
 <link href="css/style.css" rel="stylesheet" type="text/css" /> 
-    <div class="content-grids">
-    <H3>Nuevos: </H3>
+<div class="content-grids">
+    <H3 style="margin-top: 0px;">Nuevos: </H3>
     <? $key = 0?>
     @foreach($movies as $movie)
     <? ++$key ?>
     @if ($key == 4)
-        <div class="content-grid last-grid">
-    @else
+    <div class="content-grid last-grid">
+        @else
         <div class="content-grid">
-    @endif
+            @endif
             <a href="{{ action("MovieController@show", array($movie->id)) }}"><img src="files/{{$movie->imageRef}}" title="allbum-name" style="width: 220px; height: 220px;"/></a>
-            <h3 href="{{ action("MovieController@show", array($movie->id)) }}">{{$movie->name}}</h3>
+            <h3 href="{{ action("MovieController@show", array($movie->id)) }}" style="margin-top: 0px; margin-bottom: 0px;">{{$movie->name}}</h3>
             <a class="button" href="{{ action("MovieController@show", array($movie->id)) }}">Ver Ahora</a>
         </div>
         @endforeach
-        {{-- <div class="content-grid">
-            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-            <a class="button" href="singlepage.html">Watch now</a>
-        </div>
-        <div class="content-grid">
-            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-            <a class="button" href="singlepage.html">Watch now</a>
-        </div>
-        <div class="content-grid last-grid">
-            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-            <a class="button" href="singlepage.html">Watch now</a>
-        </div> --}}
-        <BR>
+
         <H3>Mas Vistos: </H3>
         <div class="content-grid">
             <a href="singlepage.html"><img src="images/gridallbum1.jpg" title="allbum-name" /></a>
@@ -57,8 +42,52 @@
             <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
             <a class="button" href="singlepage.html">Watch now</a>
         </div>
+
+        <H3>DRAMA </H3>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum1.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid last-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+
+        <H3>ACCIÓN </H3>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum1.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
+        <div class="content-grid last-grid">
+            <a href="singlepage.html"><img src="images/gridallbum2.jpg" title="allbum-name" /></a>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <a class="button" href="singlepage.html">Watch now</a>
+        </div>
     </div>
-@endsection
+    @endsection
 
 {{--<div class="review-content"> 
 <div class="reviews-section">
