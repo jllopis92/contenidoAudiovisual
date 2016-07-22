71d73f8555f6957febf4be37df8d21e174b9cae6
José Llopis <j.llopis.92@gmail.com>
2016-07-22 14:25:08 -0400

    parrilla programática primera interacción

--- app/Http/Controllers/CineTvController.php
+++ app/Http/Controllers/CineTvController.php
@@ -3,13 +3,15 @@
 namespace contenidoAudiovisual\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use contenidoAudiovisual\Movie;
 use contenidoAudiovisual\Http\Requests;
 
 class CineTvController extends Controller
 {
     public function index()
     {
-        return view('cineTv.index');
+    	$movies = Movie::Movies();
+    	return view ('cineTv.index',compact('movies'));
+        //return view('cineTv.index');
     }
 }
--- resources/views/cineTv/index.blade.php
+++ resources/views/cineTv/index.blade.php
@@ -12,6 +12,27 @@
 
 	$(document).ready(function() {
 
+		var zone = "05:30";  //Change this to your timezone
+
+	$.ajax({
+		url: 'process.php',
+        type: 'POST', // Send post data
+        data: 'type=fetch',
+        async: false,
+        success: function(s){
+        	json_events = s;
+        }
+	});
+
+
+	var currentMousePos = {
+	    x: -1,
+	    y: -1
+	};
+		jQuery(document).on("mousemove", function (event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+    });
 
 		/* initialize the external events
 		-----------------------------------------------------------------*/
@@ -38,22 +59,152 @@
 		-----------------------------------------------------------------*/
 
 		$('#calendar').fullCalendar({
+			events: JSON.parse(json_events),
+			//events: [{"id":"14","title":"New Event","start":"2015-01-24T16:00:00+04:00","allDay":false}],
+			utc: true,
 			header: {
 				left: 'prev,next today',
 				center: 'title',
 				right: 'month,agendaWeek,agendaDay'
 			},
 			editable: true,
-			droppable: true, // this allows things to be dropped onto the calendar
-			drop: function() {
-				// is the "remove after drop" checkbox checked?
-				if ($('#drop-remove').is(':checked')) {
-					// if so, remove the element from the "Draggable Events" list
-					$(this).remove();
+			droppable: true, 
+			slotDuration: '00:10:00',
+			eventReceive: function(event){
+				var title = event.title;
+				var start = event.start.format("YYYY-MM-DD[T]HH:mm:SS");
+				$.ajax({
+		    		url: 'process.php',
+		    		data: 'type=new&title='+title+'&startdate='+start+'&zone='+zone,
+		    		type: 'POST',
+		    		dataType: 'json',
+		    		success: function(response){
+		    			event.id = response.eventid;
+		    			$('#calendar').fullCalendar('updateEvent',event);
+		    		},
+		    		error: function(e){
+		    			console.log(e.responseText);
+
+		    		}
+		    	});
+				$('#calendar').fullCalendar('updateEvent',event);
+				console.log(event);
+			},
+			eventDrop: function(event, delta, revertFunc) {
+		        var title = event.title;
+		        var start = event.start.format();
+		        var end = (event.end == null) ? start : event.end.format();
+		        $.ajax({
+					url: 'process.php',
+					data: 'type=resetdate&title='+title+'&start='+start+'&end='+end+'&eventid='+event.id,
+					type: 'POST',
+					dataType: 'json',
+					success: function(response){
+						if(response.status != 'success')		    				
+						revertFunc();
+					},
+					error: function(e){		    			
+						revertFunc();
+						alert('Error processing your request: '+e.responseText);
+					}
+				});
+		    },
+		    eventClick: function(event, jsEvent, view) {
+		    	console.log(event.id);
+		          var title = prompt('Event Title:', event.title, { buttons: { Ok: true, Cancel: false} });
+		          if (title){
+		              event.title = title;
+		              console.log('type=changetitle&title='+title+'&eventid='+event.id);
+		              $.ajax({
+				    		url: 'process.php',
+				    		data: 'type=changetitle&title='+title+'&eventid='+event.id,
+				    		type: 'POST',
+				    		dataType: 'json',
+				    		success: function(response){	
+				    			if(response.status == 'success')			    			
+		              				$('#calendar').fullCalendar('updateEvent',event);
+				    		},
+				    		error: function(e){
+				    			alert('Error processing your request: '+e.responseText);
+				    		}
+				    	});
+		          }
+			},
+			eventResize: function(event, delta, revertFunc) {
+				console.log(event);
+				var title = event.title;
+				var end = event.end.format();
+				var start = event.start.format();
+		        $.ajax({
+					url: 'process.php',
+					data: 'type=resetdate&title='+title+'&start='+start+'&end='+end+'&eventid='+event.id,
+					type: 'POST',
+					dataType: 'json',
+					success: function(response){
+						if(response.status != 'success')		    				
+						revertFunc();
+					},
+					error: function(e){		    			
+						revertFunc();
+						alert('Error processing your request: '+e.responseText);
+					}
+				});
+		    },
+			eventDragStop: function (event, jsEvent, ui, view) {
+			    if (isElemOverDiv()) {
+			    	var con = confirm('Are you sure to delete this event permanently?');
+			    	if(con == true) {
+						$.ajax({
+				    		url: 'process.php',
+				    		data: 'type=remove&eventid='+event.id,
+				    		type: 'POST',
+				    		dataType: 'json',
+				    		success: function(response){
+				    			console.log(response);
+				    			if(response.status == 'success'){
+				    				$('#calendar').fullCalendar('removeEvents');
+            						getFreshEvents();
+            					}
+				    		},
+				    		error: function(e){	
+				    			alert('Error processing your request: '+e.responseText);
+				    		}
+			    		});
+					}   
 				}
 			}
 		});
 
+	function getFreshEvents(){
+		$.ajax({
+			url: 'process.php',
+	        type: 'POST', // Send post data
+	        data: 'type=fetch',
+	        async: false,
+	        success: function(s){
+	        	freshevents = s;
+	        }
+		});
+		$('#calendar').fullCalendar('addEventSource', JSON.parse(freshevents));
+	}
+
+
+	function isElemOverDiv() {
+        var trashEl = jQuery('#trash');
+
+        var ofs = trashEl.offset();
+
+        var x1 = ofs.left;
+        var x2 = ofs.left + trashEl.outerWidth(true);
+        var y1 = ofs.top;
+        var y2 = ofs.top + trashEl.outerHeight(true);
+
+        if (currentMousePos.x >= x1 && currentMousePos.x <= x2 &&
+            currentMousePos.y >= y1 && currentMousePos.y <= y2) {
+            return true;
+        }
+        return false;
+    }
 
 	});
 
@@ -65,6 +216,14 @@
 		text-align: center;
 		font-size: 14px;
 		font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+	}
+
+	#trash{
+		width:32px;
+		height:32px;
+		float:left;
+		padding-bottom: 15px;
+		position: relative;
 	}
 		
 	#wrap {
@@ -114,21 +273,20 @@
 	<div id='wrap'>
 
 		<div id='external-events'>
-			<h4>Draggable Events</h4>
-			<div class='fc-event'>My Event 1</div>
-			<div class='fc-event'>My Event 2</div>
-			<div class='fc-event'>My Event 3</div>
-			<div class='fc-event'>My Event 4</div>
-			<div class='fc-event'>My Event 5</div>
+			<h4>Videos</h4>
+			@foreach($movies as $movie)
+			<div class='fc-event'>{{$movie->name}}</div>
+			@endforeach
 			<p>
-				<input type='checkbox' id='drop-remove' />
-				<label for='drop-remove'>remove after drop</label>
+				<img src="img/trashcan.png" id="trash" alt="">
 			</p>
 		</div>
 
 		<div id='calendar'></div>
 
 		<div style='clear:both'></div>
+
+		<xspan class="tt">x</xspan>
 
 	</div>
 </body>
